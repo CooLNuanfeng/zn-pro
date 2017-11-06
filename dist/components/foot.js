854f970a22b780e8c01b262f33de5e0ae743ebf1
CooLNuanfeng <1634898698@qq.com>
2017-11-06 11:32:24 +0800

    update

---
+++ dist/components/foot.js
@@ -0,0 +1,44 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = undefined;
+
+var _wepy = require('./../npm/wepy/lib/wepy.js');
+
+var _wepy2 = _interopRequireDefault(_wepy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Location = function (_wepy$component) {
+    _inherits(Location, _wepy$component);
+
+    function Location() {
+        var _ref;
+
+        var _temp, _this, _ret;
+
+        _classCallCheck(this, Location);
+
+        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+            args[_key] = arguments[_key];
+        }
+
+        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Location.__proto__ || Object.getPrototypeOf(Location)).call.apply(_ref, [this].concat(args))), _this), _this.props = {
+            cityname: String,
+            cityid: Number
+        }, _temp), _possibleConstructorReturn(_this, _ret);
+    }
+
+    return Location;
+}(_wepy2.default.component);
+
+exports.default = Location;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZvb3QuanMiXSwibmFtZXMiOlsiTG9jYXRpb24iLCJwcm9wcyIsImNpdHluYW1lIiwiU3RyaW5nIiwiY2l0eWlkIiwiTnVtYmVyIiwiY29tcG9uZW50Il0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQ0E7Ozs7Ozs7Ozs7OztJQUNxQkEsUTs7Ozs7Ozs7Ozs7Ozs7OExBQ2pCQyxLLEdBQVE7QUFDSkMsc0JBQVdDLE1BRFA7QUFFSkMsb0JBQVNDO0FBRkwsUzs7OztFQUQwQixlQUFLQyxTOztrQkFBdEJOLFEiLCJmaWxlIjoiZm9vdC5qcyIsInNvdXJjZXNDb250ZW50IjpbIlxuaW1wb3J0IHdlcHkgZnJvbSAnd2VweSdcbmV4cG9ydCBkZWZhdWx0IGNsYXNzIExvY2F0aW9uIGV4dGVuZHMgd2VweS5jb21wb25lbnR7XG4gICAgcHJvcHMgPSB7XG4gICAgICAgIGNpdHluYW1lIDogU3RyaW5nLFxuICAgICAgICBjaXR5aWQgOiBOdW1iZXJcbiAgICB9XG59XG4iXX0=
---
+++ dist/components/footer.js
@@ -0,0 +1,44 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = undefined;
+
+var _wepy = require('./../npm/wepy/lib/wepy.js');
+
+var _wepy2 = _interopRequireDefault(_wepy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Footer = function (_wepy$component) {
+    _inherits(Footer, _wepy$component);
+
+    function Footer() {
+        var _ref;
+
+        var _temp, _this, _ret;
+
+        _classCallCheck(this, Footer);
+
+        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+            args[_key] = arguments[_key];
+        }
+
+        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Footer.__proto__ || Object.getPrototypeOf(Footer)).call.apply(_ref, [this].concat(args))), _this), _this.props = {
+            cityname: String,
+            cityid: Number
+        }, _temp), _possibleConstructorReturn(_this, _ret);
+    }
+
+    return Footer;
+}(_wepy2.default.component);
+
+exports.default = Footer;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZvb3Rlci5qcyJdLCJuYW1lcyI6WyJGb290ZXIiLCJwcm9wcyIsImNpdHluYW1lIiwiU3RyaW5nIiwiY2l0eWlkIiwiTnVtYmVyIiwiY29tcG9uZW50Il0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQ0E7Ozs7Ozs7Ozs7OztJQUNxQkEsTTs7Ozs7Ozs7Ozs7Ozs7MExBQ2pCQyxLLEdBQVE7QUFDSkMsc0JBQVdDLE1BRFA7QUFFSkMsb0JBQVNDO0FBRkwsUzs7OztFQUR3QixlQUFLQyxTOztrQkFBcEJOLE0iLCJmaWxlIjoiZm9vdGVyLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5pbXBvcnQgd2VweSBmcm9tICd3ZXB5J1xuZXhwb3J0IGRlZmF1bHQgY2xhc3MgRm9vdGVyIGV4dGVuZHMgd2VweS5jb21wb25lbnR7XG4gICAgcHJvcHMgPSB7XG4gICAgICAgIGNpdHluYW1lIDogU3RyaW5nLFxuICAgICAgICBjaXR5aWQgOiBOdW1iZXJcbiAgICB9XG59XG4iXX0=
